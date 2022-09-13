0b0b301b3710c3dc719ae6a4dbbfe81b05f977a8
JacquelynSH <elpatchaembroidery@gmail.com>
2022-09-13 13:11:47 -0700

    Refactor InterviewList and InterviewListItem

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,26 +3,24 @@ import InterviewerListItem from "./InterviewerListItem";
 import "./interviewerList.scss";
 
 export default function InterviewerList(props) {
+  console.log(props);
 
- 
-return (
-
-<section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list">
-    {props.interviewers.map((interviewer) => {
-     return (
-      <InterviewerListItem 
-        key={interviewer.id}
-        name={interviewer.name}
-        avatar={interviewer.avatar}
-        selected={props.selected}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
-      />
-     )
-})}
-  </ul>
-</section>
-
-);
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {props.interviewers.map((interviewer) => {
+          return (
+            <InterviewerListItem
+              key={interviewer.id}
+              name={interviewer.name}
+              avatar={interviewer.avatar}
+              selected={interviewer.id === props.value}
+              setInterviewer={() => props.onChange(interviewer.id)}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,29 +3,25 @@ import classNames from "classnames";
 import "./InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
-
   const interviewers = classNames("interviewer", {
-    "interviewers__item": props,
+    interviewers__item: props,
     "interviewers__item--selected": props.selected,
-  })
+  });
 
   const addName = function () {
-
     if (props.selected) {
-      return <h6>{props.name}</h6>
+      return <h6>{props.name}</h6>;
     }
-  }
+  };
 
-
-return ( 
-  <li className={interviewers} onClick={props.setInterviewer}>
-    
-  <img
-    className="interviewers__item-image" 
-    src={props.avatar}
-    alt={props.name}
-  />
-  {addName()}
-</li>
-);
+  return (
+    <li className={interviewers} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {addName()}
+    </li>
+  );
 }
