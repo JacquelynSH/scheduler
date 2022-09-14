9c86459ed0b6b60e86020be4deb958360edd701d
JacquelynSH <elpatchaembroidery@gmail.com>
2022-09-13 20:10:40 -0700

    Add confirm, error, and status components

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import InterviewerList from "../InterviewerList";
+import Button from "../Button";
+
+
+export default function Form(props) {
+
+  // const [student, setStudent] = useState(props.student || "");
+  // const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  
+  return (
+<main className="appointment__card appointment__card--create">
+  <section className="appointment__card-left">
+    <form autoComplete="off">
+      <input
+        className="appointment__create-input text--semi-bold"
+        name="name"
+        type="text"
+        placeholder="Enter Student Name"
+
+      />
+    </form>
+    <InterviewerList 
+  
+    />
+  </section>
+  <section className="appointment__card-right">
+    <section className="appointment__actions">
+      <Button danger>Cancel</Button>
+      <Button confirm>Save</Button>
+    </section>
+  </section>
+</main>
+  )
+}
