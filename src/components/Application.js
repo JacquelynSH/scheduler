02001db22da560d4e17609140ae6c88b0db60bde
JacquelynSH <elpatchaembroidery@gmail.com>
2022-09-18 13:30:32 -0700

    Make axios get request for days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,12 @@
 import "styles/Application.scss";
 import DayList from "./DayList";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios"; 
 import "components/Appointment";
 import Appointment from "components/Appointment";
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
+
 
 const appointments = {
   1: {
@@ -61,8 +48,13 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  console.log(day);
+  const [day, setDay] = useState([]);
+  
+  useEffect(() => {
+    axios.get(`http://localhost:8001/api/days`).then(response => {
+      setDay([...response.data.results]);
+    }, []);
+  });
 
   const appointmentsArray = Object.values(appointments).map(appointment => {
     return (
