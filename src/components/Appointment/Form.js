fbc1cefbef2a69149d0b54a7d56e6b1b4a6026c3
JacquelynSH <elpatchaembroidery@gmail.com>
2022-10-03 15:30:36 -0700

    Add form tests, Add validate function in Form.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,7 +7,19 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  
+  const [error, setError] = useState("");
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    props.onSave(student, interviewer);
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -19,8 +31,9 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
-           
+           data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList   
           interviewers={props.interviewers}
@@ -33,7 +46,7 @@ export default function Form(props) {
           <Button onClick={props.onCancel} danger>
             Cancel
           </Button>
-          <Button onClick={() => props.onSave(student, interviewer)} confirm>
+          <Button onClick={validate} confirm>
             Save
           </Button>
         </section>
